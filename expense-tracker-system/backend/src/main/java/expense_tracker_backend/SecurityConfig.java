3b61ff160ecbea2a766e1059b8c49f55b4ba1f9a
Anish kahar <144413430+Anishhkumarr@users.noreply.github.com>
2026-06-18 16:36:20 +0530

    Add new allowed origin for CORS configuration

--- expense-tracker-system/backend/src/main/java/expense_tracker_backend/SecurityConfig.java
+++ expense-tracker-system/backend/src/main/java/expense_tracker_backend/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "https://expense-tracker-system-silk.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
